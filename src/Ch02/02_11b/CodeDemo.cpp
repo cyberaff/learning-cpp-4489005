496909745d5b502f52a6e117f6d42f9461e8483e
Rafał Ćwikła <134967482+cyberaff@users.noreply.github.com>
2024-07-19 19:39:39 +0000

    continuing learnig

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 #include <cstdint>
 
+using namespace std;
+
 int main(){
-    
+    int farenheit = 100;
+    int celsius;
+
+    celsius = ((float)5 / 9.0) * (farenheit - 32); // casting int na float
+
+    cout << "farenheit: " << farenheit << endl;
+    cout << "Celsius  : " << celsius << endl << endl;
+
+    float weight = 10.99;
+
+    cout << "Weight = " << weight << endl;
+    cout << "Float part      : " << weight << endl;
+    cout << "Integer part    : " << (int)weight << endl;
+    cout << "Fractional part : " << (int)((weight - (int)weight) * 1000000000) << endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
