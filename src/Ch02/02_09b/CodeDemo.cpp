3499381701b5b97de0307806aa13a7f3ecf23bb9
Rafał Ćwikła <134967482+cyberaff@users.noreply.github.com>
2024-07-19 11:46:39 +0000

    added learning process

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,8 +3,23 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
 
 int main(){
+    const size_t LENGHT1 = 25;
+
+    char array_str1[LENGHT1] = "Hey guys! ";
+    char array_str2[] = "Wats up?";
+
+    string std_str1 = "Hi everybody! ";
+    string std_str2 = "How's the going?";
+
+    strncat(array_str1, array_str2, LENGHT1); // z języka c z biblioteki cstring
+    cout << array_str1 << endl;
+    cout << std_str1 + std_str2 << endl; // w c++ dajemy + jak w pythonie
     
     std::cout << std::endl << std::endl;
     return (0);
