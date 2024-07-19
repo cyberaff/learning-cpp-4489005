70dc7a473e517325ab6640f7deeea30684169ee4
Rafał Ćwikła <134967482+cyberaff@users.noreply.github.com>
2024-07-19 10:31:53 +0000

    Making changes in durin learning

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,32 @@
 
 #include <iostream>
 
+using namespace std;
+
+int a, b = 5; 
+/*
+zmienne tego samego typu możemy deklarwać w tej 
+samej linii
+*/ 
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a =7;
+    my_flag = false;
     
-    std::cout << std::endl << std::endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "flag = " << my_flag << endl << endl;
+    
+    my_flag = true; // nadpisanie zmiennej
+    cout << "flag = " << my_flag << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "b - a = " << b - a  << endl << endl;
+
+    unsigned int positive;
+    positive = b - a;
+    cout << "b - a (unsigned) = " << positive << endl << endl; 
+
+
     return (0);
 }
--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,25 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+// Preprocesory
+#define CAPACITY 5000
+#define DEBUG // Definicja preprocesora przydatnego do debugowania kodu
+
+using namespace std;
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+// ifdef - warunek jeżeli preprocesor jest zdefiniowany to ta częć kodu jest wykonywana
+#ifdef DEBUG 
+    cout << "[About to perform the addition" << endl;
+#endif
+    large += small; // ekwiwalent dla zpisu large = large + small
+    cout << "The large integer is " << large << endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
