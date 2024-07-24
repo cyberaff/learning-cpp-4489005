9fe8be2b1568d056621444be39d65c1d47c1ed97
Rafał Ćwikła <134967482+cyberaff@users.noreply.github.com>
2024-07-24 20:34:21 +0000

    learning process - while loops

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,7 +8,24 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator ptr = numbers.begin();
+    // std::vector<int>::iterator ptr = numbers.begin();
+    // zamiast deklarować zmienną std::vector<int>::iterator możemy użyć typu auto
+    auto ptr = numbers.begin(); // zaczynamy iterować od początku listy
+
+    // pętla while
+    while (ptr != numbers.end()){
+        std::cout << *ptr << " ";
+        ptr = next(ptr, 1);
+    }
+    std::cout << std::endl;
+
+    // pętla do - while konstrukcja "do something whie condition"
+    int i = 0;
+    do{  // określamy co ma tu robić pętla
+        std::cout << numbers[i] << " ";
+        i++;
+    }while (i < numbers.size()); // tu określamy warunek do kiedy ma ta pętla działać
+    std::cout << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
