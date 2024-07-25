f197ab1c83ec850bef50181b22b9f9b726aa24b2
Rafał Ćwikła <134967482+cyberaff@users.noreply.github.com>
2024-07-25 19:29:40 +0000

    learning process

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -9,6 +9,25 @@ int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     float average;
     
+    average = 0;
+    // for loop - inicjalizujemy zmienną iteracyjną; warunek; inkrementacja;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        average += numbers[i];
+    }
+    average /= numbers.size();
+
+    std::cout << "Average: " << average << std::endl;
+
+    // inny sposób zapisu pętli for
+    average = 0;
+    for (int x : numbers){
+        average += x;
+    }
+    average /= numbers.size();
+
+    std::cout << "Average: " << average << std::endl;
+    
     std::cout << std::endl << std::endl;
     return (0);
 }
